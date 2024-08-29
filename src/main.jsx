eaea5020c1b62fdec4f6b7c8ee0ab45fb90111e0
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-08-29 20:40:07 -0300

    fix provider store

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-createRoot(document.getElementById("root")).render(<App />);
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
